2769d48803e07d304c467ee659d7a86c329db512
Alexandre <xan.marconi@hotmail.com>
2016-06-06 14:48:01 -0300

    ínital commit

---
+++ eSports/eSports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eSports
+//
+//  Created by Alexandre Calil Marconi on 6/6/16.
+//  Copyright © 2016 Alexandre Calil Marconi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
