748b6d6cbfd44fce5a1d5478beb36ed60c1c204b
Alexandre <xan.marconi@hotmail.com>
2016-06-15 15:44:40 -0300

    update tournmanet UI

--- eSports/eSports/GamesCollectionViewCell.swift
+++ eSports/eSports/GamesCollectionViewCell.swift
@@ -10,43 +10,17 @@ import UIKit
 
 class GamesCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var title: UILabel!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var topConstraint: NSLayoutConstraint!
     
     var currentGame: Discipline?
     
-    
     override func awakeFromNib() {
-        
         imageView.adjustsImageWhenAncestorFocused = true
-        
-    }
-    
-    
-    override func didUpdateFocusInContext(context: UIFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
-        if (context.nextFocusedView == self) {
-            
-            self.topConstraint.constant = 80
-            UIView.animateWithDuration(0.3) {
-                self.title.transform = CGAffineTransformMakeScale(1.4, 1.4)
-                self.layoutIfNeeded()
-            }
-            
-        } else {
-            self.topConstraint.constant = 30
-            UIView.animateWithDuration(0.3) {
-                self.title.transform = CGAffineTransformMakeScale(1.0, 1.0)
-                self.layoutIfNeeded()
-            }
-
-        }
     }
     
     func configureCell(discipline: Discipline) {
         currentGame = discipline
         
         imageView.image = discipline.image
-        title.text = discipline.name
     }
 }
