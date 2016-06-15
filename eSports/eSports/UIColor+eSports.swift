f76d69d8a87f85bee7ad6771c1b2ebebb95ecae3
Carlos Henrique Cayres <carloshenriquecayres@Carloss-MacBook-Pro-2.local>
2016-06-15 15:45:31 -0300

    Cores

--- eSports/eSports/UIColor+eSports.swift
+++ eSports/eSports/UIColor+eSports.swift
@@ -10,16 +10,54 @@ import UIKit
 
 extension UIColor {
     
+    
+    
+    //Gray
     class func eSports_LightGray() -> Self {
-        return self.init(red: 220.0 / 255.0, green: 220.0 / 255.0, blue: 220.0 / 255.0, alpha: 1.0)
+        return self.init(red: 150 / 255.0, green: 150 / 255.0, blue: 150 / 150, alpha: 1.0)
     }
     
     class func eSports_DarkGray() -> Self {
-        return self.init(red: 66.0 / 255.0, green: 66.0 / 255.0, blue: 66.0 / 255.0, alpha: 1.0)
+        return self.init(red: 90 / 255.0, green: 90 / 255.0, blue: 90 / 255.0, alpha: 1.0)
     }
     
-    class func eSports_Blue() -> Self {
-        return self.init(red: 21.0 / 255.0, green: 123.0 / 255.0, blue: 251.0 / 255.0, alpha: 1.0)
+    class func eSports_MediumGray() -> Self {
+        return self.init(red: 110 / 255.0, green: 110 / 255.0, blue: 110 / 255.0, alpha: 1.0)
     }
+    
+    
+    
+    
+    //Blue
+    class func eSports_DarkBlue() -> Self {
+        return self.init(red: 0 / 255.0, green: 114 / 255.0, blue: 188 / 255.0, alpha: 1.0)
+    }
+    
+    class func eSports_LightBlue() -> Self {
+        return self.init(red: 0 / 255.0, green: 158 / 255.0, blue: 226 / 255.0, alpha: 1.0)
+    }
+    
+    
+    
+    
+    //Red
+    class func eSports_DarkRed() -> Self {
+        return self.init(red: 238 / 255.0, green: 53 / 255.0, blue: 50 / 255.0, alpha: 1.0)
+    }
+    
+    class func eSports_LightRed() -> Self {
+        return self.init(red: 168 / 255.0, green: 30 / 255.0, blue: 34 / 255.0, alpha: 1.0)
+    }
+
+    
+    
+    //Black
+    class func eSports_Black() -> Self {
+        return self.init(red: 20 / 255.0, green: 20 / 255.0, blue: 20 / 255.0, alpha: 1.0)
+    }
+    
+    
+    
+    
 
 }
