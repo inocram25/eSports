6fac2c099a1c01d4b846b9ccb401c55d1354e466
guilherme babugia <babugia@icloud.com>
2016-06-21 00:17:36 -0300

    region filter feito de um jeito decente

--- eSports/eSports/TournamentsViewController.swift
+++ eSports/eSports/TournamentsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 private let reuseIdentifier = "tournamentsCell"
 
@@ -27,26 +28,64 @@ class TournamentsViewController: UIViewController {
     var discipline: Discipline?
     var region: Region?
     
-    let countryDictionary = ["NA":["AG","AI","AN","AW","BB","BL","BM","BS","BZ","CA","CR","CU","DM","DO","GD","GL","GP","GT","HN","HT","JM","KN","KY","LC","MF","MQ","MS","MX","NI","PM","PR","SV","TC","TT","VC","VG","VI","PA","US"],
-                             "EU":["AL","AT","AX","BA","BG","BY","CZ","DE","DK","EE","EU","FI","FO","FR","FX","GB","GG","GR","HR","IE","IM","IT","JE","LI","LU","LV","MD","ME","MK","MT","NL","PL","RO","RU","SI","SK","SM","TR","UA","VA","AD","LT","MC","NO","BE","PT","CH","PY","RS","SE","SJ","ES","GI","HU","IS"],
-                             "AS":["AE","AF","AM","AP","AZ","BD","BH","BN","BT","CC","CN","CX","CY","GE","HK","ID","IL","IN","IO","IQ","IR","JO","JP","KG","KH","KP","KR","KW","KZ","LA","LB","LK","MM","MN","MO","MV","MY","NP","OM","PH","PK","PS","QA","SA","SG","SY","TH","TJ","TL","TM","TW","UZ","VN","YE"],
-                             "AF":["AO","BF","BI","BJ","BW","CD","CF","CG","CI","CM","CV","DJ","DZ","EG","EH","ER","ET","GA","GH","GM","GN","GQ","GW","KE","KM","LR","LS","LY","MA","MG","ML","MR","MU","MW","MZ","NA","NE","NG","RE","RW","SC","SD","SH","SL","SN","SO","ST","SZ","TD","TG","TN","TZ","UG","YT","ZA","ZW","ZM"],
-                             "OC":["AS","AU","CK","FJ","FM","GU","KI","MH","MP","NC","NF","NR","NU","NZ","PF","PG","PN","PW","SB","TK","TO","TV","UM","VU","WF","WS"],
-                             "SA": ["AR","BO","BR","CL","CO","EC","FK","GF","GY","PE","PY","SR","UY","VE"]]
+    var continentDictionary = ["NA": [""],
+                               "EU":[""],
+                               "AS":[""],
+                               "AF":[""],
+                               "OC":[""],
+                               "SA":[""]]
+    
+//    let countryDictionary = ["NA":["AG","AI","AN","AW","BB","BL","BM","BS","BZ","CA","CR","CU","DM","DO","GD","GL","GP","GT","HN","HT","JM","KN","KY","LC","MF","MQ","MS","MX","NI","PM","PR","SV","TC","TT","VC","VG","VI","PA","US"],
+//                             "EU":["AL","AT","AX","BA","BG","BY","CZ","DE","DK","EE","EU","FI","FO","FR","FX","GB","GG","GR","HR","IE","IM","IT","JE","LI","LU","LV","MD","ME","MK","MT","NL","PL","RO","RU","SI","SK","SM","TR","UA","VA","AD","LT","MC","NO","BE","PT","CH","PY","RS","SE","SJ","ES","GI","HU","IS"],
+//                             "AS":["AE","AF","AM","AP","AZ","BD","BH","BN","BT","CC","CN","CX","CY","GE","HK","ID","IL","IN","IO","IQ","IR","JO","JP","KG","KH","KP","KR","KW","KZ","LA","LB","LK","MM","MN","MO","MV","MY","NP","OM","PH","PK","PS","QA","SA","SG","SY","TH","TJ","TL","TM","TW","UZ","VN","YE"],
+//                             "AF":["AO","BF","BI","BJ","BW","CD","CF","CG","CI","CM","CV","DJ","DZ","EG","EH","ER","ET","GA","GH","GM","GN","GQ","GW","KE","KM","LR","LS","LY","MA","MG","ML","MR","MU","MW","MZ","NA","NE","NG","RE","RW","SC","SD","SH","SL","SN","SO","ST","SZ","TD","TG","TN","TZ","UG","YT","ZA","ZW","ZM"],
+//                             "OC":["AS","AU","CK","FJ","FM","GU","KI","MH","MP","NC","NF","NR","NU","NZ","PF","PG","PN","PW","SB","TK","TO","TV","UM","VU","WF","WS"],
+//                             "SA": ["AR","BO","BR","CL","CO","EC","FK","GF","GY","PE","PY","SR","UY","VE"]]
     
     func tournamentsByContinent(continent:String, tournamentList: [Tournament]) -> [Tournament] {
         
         return tournamentList.filter { (tournament) -> Bool in
             if let country = tournament.country {
-                print("\(country) -- \(continent) = \(countryDictionary[continent]!.contains(country))")
-                return countryDictionary[continent]!.contains(country)
+//                print("\(country) -- \(continent) = \(countryDictionary[continent]!.contains(country))")
+                return continentDictionary[continent]!.contains(country)
             }
             return false
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let url = NSBundle.mainBundle().URLForResource("Countries+Continents", withExtension: "json")
+        let data = NSData(contentsOfURL: url!)
+        
+        do {
+            let object = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                let json = JSON(object)
+            
+                for (_,subJson):(String, JSON) in json {
+                    
+                    if subJson["continent"].stringValue == "NA" {
+                        continentDictionary["NA"]!.append(subJson["country"].stringValue)
+                    }
+                    if subJson["continent"].stringValue == "EU" {
+                        continentDictionary["EU"]!.append(subJson["country"].stringValue)
+                    }
+                    if subJson["continent"].stringValue == "AS" {
+                        continentDictionary["AS"]!.append(subJson["country"].stringValue)
+                    }
+                    if subJson["continent"].stringValue == "AF" {
+                        continentDictionary["AF"]!.append(subJson["country"].stringValue)
+                    }
+                    if subJson["continent"].stringValue == "SA" {
+                        continentDictionary["SA"]!.append(subJson["country"].stringValue)
+                    }
+                    
+                }
+            
+        } catch {
+            // Handle Error
+        }
         
         activityIndicator.color = UIColor.eSports_DarkBlue()
         activityIndicator.layer.hidden = false
@@ -55,7 +94,7 @@ class TournamentsViewController: UIViewController {
         regionLabel.textColor = UIColor.eSports_MediumGray()
         
         let currentDate = "\(NSDate().year)-\(NSDate().month)-\(NSDate().day)"
-        print(currentDate)
+//        print(currentDate)
         
         if let discipline = discipline {
             toornamentClient.getTournaments(discipline.id, beforeStart: currentDate, sort: "date_desc") { [weak self] result in
@@ -64,11 +103,11 @@ class TournamentsViewController: UIViewController {
                 }
                 
                 if let initial = self!.region?.initials, tournaments = self?.tournaments {
-                    print(initial)
-                    print(tournaments.count)
+//                    print(initial)
+//                    print(tournaments.count)
                     let x = self?.tournamentsByContinent(initial, tournamentList: tournaments)
                     
-                    print(x?.count)
+//                    print(x?.count)
                     
                     self?.tournaments = x!
                 }
